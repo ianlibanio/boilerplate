1ca7aa4e28d1a367b8ff0f759a3f1d48e2428b3e
Ian Libânio <huetroll1@gmail.com>
2021-07-16 15:27:20 -0300

    pwa integration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
+  reactStrictMode: true
+})
