3c4ea755519507c9c60fe832ca0f55c29efc61a3
Ian Libânio <huetroll1@gmail.com>
2021-07-19 15:21:17 -0300

    feat: add open graph tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,25 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="A boilerplate to work with TypeScript, NextJS and Styled Components"
         />
+
+        {/* Open Graph */}
+        <meta
+          property="og:url"
+          content="https://boilerplate.ianlibanio.com"
+          key="ogurl"
+        />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/ianlibanio/boilerplate/main/public/img/banner.png"
+          key="ogimage"
+        />
+        <meta property="og:site_name" content="Boilerplate" key="ogsitename" />
+        <meta property="og:title" content="Next.JS Boilerplate" key="ogtitle" />
+        <meta
+          property="og:description"
+          content="A boilerplate to work with TypeScript, NextJS and Styled Components"
+          key="ogdesc"
+        />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
