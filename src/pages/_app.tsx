04be1b88d65c80614e890f3ec98c50e84853fc08
Ian Libânio <huetroll1@gmail.com>
2021-07-21 14:02:23 -0300

    use next-seo to set head tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,14 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
+
+import { DefaultSeo } from 'next-seo'
+import SEO from '../../next-seo.config'
 
 import { GlobalStyle } from 'styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Next.JS Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#0d1117" />
-        <meta
-          name="description"
-          content="A boilerplate to work with TypeScript, NextJS and Styled Components"
-        />
-
-        {/* Open Graph */}
-        <meta
-          property="og:url"
-          content="https://boilerplate.ianlibanio.com"
-          key="ogurl"
-        />
-        <meta
-          property="og:image"
-          content="https://raw.githubusercontent.com/ianlibanio/boilerplate/main/public/img/banner.png"
-          key="ogimage"
-        />
-        <meta property="og:site_name" content="Boilerplate" key="ogsitename" />
-        <meta property="og:title" content="Next.JS Boilerplate" key="ogtitle" />
-        <meta
-          property="og:description"
-          content="A boilerplate to work with TypeScript, NextJS and Styled Components"
-          key="ogdesc"
-        />
-      </Head>
+      <DefaultSeo {...SEO} />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
